a094fc4f6bcc9746e1d3515cbc186ce7c6dd5f37
Oliwier <m0lc14kk@outlook.com>
2025-12-13 13:54:30 +0100

    chore(ci->tests): added CI workflow tests

---
+++ tests/SampleTest.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "node:test"
+import assert from "node:assert"
+
+describe("SampleTest", () => {
+    it("should pass", () => {
+        assert.strictEqual(true, true)
+    })
+})
