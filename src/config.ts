21fc350e2113c3311c5a64eefd398145df5028d2
Jakub <45879657+zeedivx@users.noreply.github.com>
2026-01-06 16:18:38 +0100

    fix(config): update DEFAULT_AUTHORIZE_PATH to remove versioning from the URL

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_JWKS_PATH = "/.well-known/jwks.json"
-export const DEFAULT_AUTHORIZE_PATH = "/v1/oauth/authorize"
+export const DEFAULT_AUTHORIZE_PATH = "/authorize"
 export const DEFAULT_ALGORITHMS = ["RS256"]
