e82219a1959fb6ed41b880155dcef9c2a052b5e2
Oliwier <m0lc14kk@outlook.com>
2026-01-07 13:25:21 +0100

    docs(scripts): documented script for package deployment

--- scripts/DeployScript.ts
+++ scripts/DeployScript.ts
@@ -3,14 +3,30 @@ import { writeFile } from "fs/promises"
 import { readFile } from "fs/promises"
 import { join } from "path"
 
+/**
+ * @summary This script is used to deploy the package to the NPM registry.
+ */
 export class DeployScript {
+    /**
+     * @summary Private constructor to prevent instantiation.
+     */
     private constructor() {}
+
+    /**
+     * @summary The path to the package.json file.
+     */
     private static readonly PACKAGE_PATH: string = join(process.cwd(), "package.json")
 
+    /**
+     * @summary Initializes the script.
+     */
     static {
         void this.init()
     }
 
+    /**
+     * @summary Initializes the script.
+     */
     private static async init(): Promise<void> {
         const fileContent: Record<string, unknown> = JSON.parse(await readFile(this.PACKAGE_PATH, "utf-8"))
         fileContent.version = argv[1].replace(/^v/, "")
